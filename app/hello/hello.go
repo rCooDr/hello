3d9831614bcc17f5df3fd752e796a86d9481a41a
glisze <gunter.liszewski@gmail.com>
2013-11-23 19:00:51 +0000

    feed the line at the end of that massage

--- app/hello/hello.go
+++ app/hello/hello.go
@@ -10,5 +10,5 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hi, Lo, this is the plat formed app-engine!")
+    fmt.Fprint(w, "Hi, Lo, this is the plat formed app-engine!\n")
 }
