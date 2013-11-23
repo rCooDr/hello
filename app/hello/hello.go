fb300627ca6d4d9d49f743453c545bdfa1d7bf10
glisze <gunter.liszewski@gmail.com>
2013-11-23 18:59:10 +0000

    pastin from google developers

---
+++ app/hello/hello.go
@@ -0,0 +1,14 @@
+package hello
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func init() {
+    http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hi, Lo, this is the plat formed app-engine!")
+}
