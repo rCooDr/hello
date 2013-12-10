7701a5e1876305d7c7105aeba695c32d764acda8
glisze <gunter.liszewski@gmail.com>
2013-12-10 18:27:33 +0000

    pasted into the appspot thingamagic...ally

--- app/app1/hello.go
+++ app/app1/hello.go
@@ -2,12 +2,32 @@ package hello
  
 import (
     "fmt"
+    "io/ioutil"
     "net/http"
  
     "appengine"
     "appengine/user"
     "github.com/rCooDr/onemath"
 )
+
+type Page struct {
+    Title string
+    Body  []byte
+}
+
+func (p *Page) save() error {
+    filename := p.Title + ".txt"
+    return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
  
 func init() {
     http.HandleFunc("/get", get)
@@ -34,6 +54,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
  
 func get(w http.ResponseWriter, r *http.Request) {
+
+    p2, _ := loadPage("TestPage")
+
     c := appengine.NewContext(r)
     u := user.Current(c)
     if u == nil {
@@ -46,10 +69,15 @@ func get(w http.ResponseWriter, r *http.Request) {
         w.WriteHeader(http.StatusFound)
         return
     }
-    fmt.Fprintf(w, "Hello, %v!", u)
+    fmt.Fprintf(w, "<pre>Hello, %v!</pre><hr/>", u)
+    fmt.Println(string(p2.Body))
 }
  
 func put(w http.ResponseWriter, r *http.Request) {
+
+    p1 := &Page{Title: "TestPage", Body: []byte("<h1>This is a sample Page.</h1>\n<p>squeezed into the app</p>")}
+    p1.save()
+
     c := appengine.NewContext(r)
     u := user.Current(c)
     if u == nil {
