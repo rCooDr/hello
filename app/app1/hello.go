4e20b15588b671c413637d3760c7dc7e6b974215
glisze <gunter.liszewski@gmail.com>
2013-12-10 18:11:34 +0000

    copied the appspot app hello.go -- to expand with go tutorial examples

---
+++ app/app1/hello.go
@@ -0,0 +1,98 @@
+package hello
+ 
+import (
+    "fmt"
+    "net/http"
+ 
+    "appengine"
+    "appengine/user"
+    "github.com/rCooDr/onemath"
+)
+ 
+func init() {
+    http.HandleFunc("/get", get)
+    http.HandleFunc("/put", put)
+    http.HandleFunc("/post", post)
+    http.HandleFunc("/delete", delete)
+    http.HandleFunc("/Sqrt", handler)
+}
+ 
+func handler(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u == nil {
+        url, err := user.LoginURL(c, r.URL.String())
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Location", url)
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+    fmt.Fprintf(w, "<h1>Hello,</h1>\n<p>%v!</p><pre>Sqrt(%v) = %v</pre>", u, 4, onemath.Sqrt(4))
+}
+ 
+func get(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u == nil {
+        url, err := user.LoginURL(c, r.URL.String())
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Location", url)
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+    fmt.Fprintf(w, "Hello, %v!", u)
+}
+ 
+func put(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u == nil {
+        url, err := user.LoginURL(c, r.URL.String())
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Location", url)
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+    fmt.Fprintf(w, "<h1>put</h1> <p>%v!</p>", u)
+}
+ 
+func post(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u == nil {
+        url, err := user.LoginURL(c, r.URL.String())
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Location", url)
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+    fmt.Fprintf(w, "<h1>post</h1><p>%v!</p>", u)
+}
+ 
+func delete(w http.ResponseWriter, r *http.Request) {
+    c := appengine.NewContext(r)
+    u := user.Current(c)
+    if u == nil {
+        url, err := user.LoginURL(c, r.URL.String())
+        if err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+            return
+        }
+        w.Header().Set("Location", url)
+        w.WriteHeader(http.StatusFound)
+        return
+    }
+    fmt.Fprintf(w, "<h1>delete</h1> <p>%v!</p>", u)
+~/qnjp-uQ-Vn6/app/hello$
